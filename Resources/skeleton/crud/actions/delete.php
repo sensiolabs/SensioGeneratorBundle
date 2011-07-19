e18ab78a18ed376a3d0116ed667292352a262883
Hugo Hamon <hugo.hamon@sensio.com>
2011-07-19 17:27:27 +0200

    [Resources] simplified delete action skeleton.

--- Resources/skeleton/crud/actions/delete.php
+++ Resources/skeleton/crud/actions/delete.php
@@ -12,20 +12,18 @@
         $form = $this->createDeleteForm($id);
         $request = $this->getRequest();
 
-        if ('POST' === $request->getMethod()) {
-            $form->bindRequest($request);
+        $form->bindRequest($request);
 
-            if ($form->isValid()) {
-                $em = $this->getDoctrine()->getEntityManager();
-                $entity = $em->getRepository('{{ bundle }}:{{ entity }}')->find($id);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getEntityManager();
+            $entity = $em->getRepository('{{ bundle }}:{{ entity }}')->find($id);
 
-                if (!$entity) {
-                    throw $this->createNotFoundException('Unable to find {{ entity }} entity.');
-                }
-
-                $em->remove($entity);
-                $em->flush();
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find {{ entity }} entity.');
             }
+
+            $em->remove($entity);
+            $em->flush();
         }
 
         return $this->redirect($this->generateUrl('{{ route_name_prefix }}'));
