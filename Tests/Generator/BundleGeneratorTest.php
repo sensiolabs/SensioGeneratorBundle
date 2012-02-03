2f79b9450d22935426f3918852fba7992c34df16
danielsan80 <danilo.sanchi@gmail.com>
2012-02-03 17:34:03 +0100

    changed namespace used

--- Tests/Generator/BundleGeneratorTest.php
+++ Tests/Generator/BundleGeneratorTest.php
@@ -12,7 +12,7 @@
 namespace Sensio\Bundle\GeneratorBundle\Tests\Generator;
 
 use Sensio\Bundle\GeneratorBundle\Generator\BundleGenerator;
-use Symfony\Component\HttpKernel\Util\Filesystem;
+use Symfony\Component\Filesystem\Filesystem;
 
 class BundleGeneratorTest extends GeneratorTest
 {
