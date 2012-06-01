27ab745ca75141a0f2bd344dc2df8016153d5d8c
nomack84 <nomack841024@googlemail.com>
2012-06-01 12:19:06 -0400

    Added some improvements to the GenerateFormCommand

--- Resources/skeleton/form/FormType.php
+++ Resources/skeleton/form/FormType.php
@@ -3,11 +3,12 @@
 namespace {{ namespace }}\Form{{ entity_namespace ? '\\' ~ entity_namespace : '' }};
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class {{ form_class }} extends AbstractType
 {
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
         {%- for field in fields %}
@@ -17,6 +18,13 @@ class {{ form_class }} extends AbstractType
         {%- endfor %}
 
         ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => '{{ namespace }}\Entity{{ entity_namespace ? '\\' ~ entity_namespace : '' }}\{{ entity_class }}'
+        ));
     }
 
     public function getName()
