6fe3188b5cef04c32a0acdc8020be487042bce7d
Ryan Weaver <ryan@thatsquality.com>
2014-11-25 17:17:46 -0500

    Allowing a namespace to be used without a vendor namespace

--- Command/Validators.php
+++ Command/Validators.php
@@ -18,7 +18,7 @@ namespace Sensio\Bundle\GeneratorBundle\Command;
  */
 class Validators
 {
-    public static function validateBundleNamespace($namespace)
+    public static function validateBundleNamespace($namespace, $requireVendorNamespace = true)
     {
         if (!preg_match('/Bundle$/', $namespace)) {
             throw new \InvalidArgumentException('The namespace must end with Bundle.');
@@ -38,7 +38,8 @@ class Validators
         }
 
         // validate that the namespace is at least one level deep
-        if (false === strpos($namespace, '\\')) {
+        if ($requireVendorNamespace && false === strpos($namespace, '\\')) {
+            // language is (almost) duplicated in GenerateBundleCommand
             $msg = array();
             $msg[] = sprintf('The namespace must contain a vendor namespace (e.g. "VendorName\%s" instead of simply "%s").', $namespace, $namespace);
             $msg[] = 'If you\'ve specified a vendor namespace, did you forget to surround it with quotes (init:bundle "Acme\BlogBundle")?';
