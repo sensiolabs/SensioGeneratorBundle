76d1c52100656b0ea9ad9e13766a31b12879f27b
Javier Eguiluz <javier.eguiluz@gmail.com>
2015-08-10 10:05:07 +0200

    Addee back some tests removed by mistake

--- Tests/Generator/DoctrineFormGeneratorTest.php
+++ Tests/Generator/DoctrineFormGeneratorTest.php
@@ -23,6 +23,9 @@ class DoctrineFormGeneratorTest extends GeneratorTest
 
         $content = file_get_contents($this->tmpDir.'/Form/PostType.php');
         $this->assertContains('->add(\'title\')', $content);
+        $this->assertContains('->add(\'createdAt\', \'date\')', $content);
+        $this->assertContains('->add(\'publishedAt\', \'time\')', $content);
+        $this->assertContains('->add(\'updatedAt\', \'datetime\')', $content);
         $this->assertContains('class PostType extends AbstractType', $content);
         $this->assertContains("'data_class' => 'Foo\BarBundle\Entity\Post'", $content);
         $this->assertContains("'foo_barbundle_post'", $content);
